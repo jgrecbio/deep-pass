cd2bcf95ddca849288b6fdb7f49ef56284809e25
jgrec <jgrec.bio@gmail.com>
2020-02-23 17:06:04 +0100

    Downloads data

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import argparse
 import boto3 as boto
 
 
@@ -8,3 +9,14 @@ def get_client():
 
 def get_dataset(client, bucket, key):
     client.download(bucket, key, key)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bucket")
+    parser.add_argument("-k", "--key")
+
+    args = parser.parse_args()
+
+    client = get_client()
+    get_dataset(client, args.bucket, args.key)
