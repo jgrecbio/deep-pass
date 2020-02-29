0ee06a6f4f1b6dfa719b66020017ee7f9dd60765
jgrec <jgrec.bio@gmail.com>
2020-02-29 12:18:07 +0100

    Vectorizes for rnn seq2seq

--- vectorization.py
+++ vectorization.py
@@ -9,14 +9,23 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 def get_label_vectorizer(psswds: List[str],
+                         start_end: bool = False,
                          vocabulary_size: int = 2048) -> Tuple[Dict[str, int],
                                                                Dict[int, str]]:
+    if start_end:
+        st = 3
+    else:
+        st = 1
     counter = Counter(concat(psswds))
-    label2index = {x[0]: i + 1
+    label2index = {x[0]: i
                    for i, x
-                   in enumerate(counter.most_common(vocabulary_size - 1))}
-    label2index["unk"] = 2049
+                   in enumerate(st, counter.most_common(vocabulary_size + st))}
+    label2index["unk"] = min(vocabulary_size + st,
+                             len(counter.most_common()))
     label2index["pad"] = 0
+    if start_end:
+        label2index["<start>"] = 1
+        label2index["<end>"] = 2
     index2label = {i: label for (label, i) in label2index.items()}
     return label2index, index2label
 
@@ -73,6 +82,31 @@ def construct_batches(x: np.ndarray,
     np.random.shuffle(x)
     for i in range(0, len(x), batch_size):
         yield x[i: i + batch_size]
+
+
+def vectorizes_sequence(psswds: Iterable[str],
+                        l2i: Dict[str, int],
+                        ohe: OneHotEncoder,
+                        maxlen: int) -> Tuple[np.ndarray, np.ndarray]:
+    features = []
+    labels = []
+    for psswd in psswds:
+        features.append(list(map(lambda x: l2i.get(x, l2i["unk"]),
+                                 ["<start>"] + list(psswd))))
+        labels.append(list(map(lambda x: l2i.get(x, l2i["unk"]),
+                               list(psswd) + ["<end>"])))
+    padded_features = pad_sequences(features,
+                                    maxlen=maxlen,
+                                    padding="post",
+                                    truncating="post",
+                                    value=l2i.get("pad", 0))
+    padded_labels = pad_sequences(labels,
+                                  maxlen=maxlen,
+                                  padding="post",
+                                  truncating="post",
+                                  value=l2i.get("pad", 0))
+    return padded_features, padded_labels
+
 
 
 if __name__ == "__main__":
