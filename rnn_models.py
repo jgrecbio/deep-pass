d7ec8224718e8d5756bbaed7030c93d315ec2031
jgrec <jgrec.bio@gmail.com>
2020-02-29 11:45:13 +0100

    Creates and compiles RNN model

---
+++ rnn_models.py
@@ -0,0 +1,37 @@
+from tf.keras.optimizers import Optimizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (LSTM, Dense, TimeDistributed, Embedding,
+                                     Bidirectional)
+
+
+def get_rnn_model(vocab_size: int,
+                  embedding_dim: int,
+                  units: int,
+                  neurons: int,
+                  dropout: float) -> Sequential:
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size,
+                        output_dim=embedding_dim))
+    model.add(Bidirectional(LSTM(units=units,
+                                 return_sequences=True,
+                                 dropout=dropout,
+                                 recurrent_dropout=dropout)))
+    model.add(Bidirectional(LSTM(units=units,
+                                 return_sequences=True,
+                                 dropout=dropout,
+                                 recurrent_dropout=dropout)))
+    model.add(LSTM(units=units,
+                   return_sequences=True,
+                   dropout=dropout,
+                   recurrent_dropout=dropout))
+    model.add(TimeDistributed(Dense(neurons, activation="relu")))
+    model.add(TimeDistributed(Dense(vocab_size, activation="softmax")))
+    return model
+
+
+def compile_model(model: Sequential,
+                  optimizer: Optimizer) -> Sequential:
+    model.compile(optimizer=optimizer,
+                  metrics="accuracy",
+                  loss="categorical_crossentropy")
+    return model
