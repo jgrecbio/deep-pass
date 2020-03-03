8bb6b6372f0e39650b3c650e5125f2a68cfdf409
jgrec <jgrec.bio@gmail.com>
2020-03-03 16:36:08 +0100

    Generates password in eager mode

--- generate_passwords.py
+++ generate_passwords.py
@@ -7,16 +7,28 @@ def generate_random_psswds(model: Sequential,
                            l2i: Dict[str, int],
                            i2l: Dict[int, str],
                            psswd_length: int,
-                           batch_size: int,
-                           depth: int,
                            temperature: float) -> List[str]:
     input_eval = ["<start>"]
     for i in range(psswd_length):
-        vec_input_eval = tf.convert_to_tensor([l2i[x] for x in input_eval])
-        predictions = model(vec_input_eval.reshape((1, -1)))
+        vec_input_eval = tf.convert_to_tensor([[l2i[x] for x in input_eval]])
+        predictions = model(vec_input_eval)
         predictions = tf.squeeze(predictions, 0)
         predictions = predictions / temperature
         predicted_id = tf.random.categorical(predictions,
                                              num_samples=1)[-1, 0].numpy()
-        input_eval.append(i2l[int(predicted_id)])
+        input_eval.append(i2l[str(predicted_id)])
+    return "".join(input_eval)
+
+
+def generate_eager(model: Sequential,
+                   l2i: Dict[str, int],
+                   i2l: Dict[int, str],
+                   psswd_length: int) -> str:
+    input_eval = ["<start>"]
+    for i in range(psswd_length):
+        vec_input_eval = tf.convert_to_tensor([[l2i[x] for x in input_eval]])
+        predictions = model(vec_input_eval)
+        predictions = tf.squeeze(predictions, 0)
+        pred_id = tf.argmax(predictions, axis=-1)[-1].numpy()
+        input_eval.append(i2l[str(pred_id)])
     return "".join(input_eval)
